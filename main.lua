aceb8c69f6b455617efe548f50cde6688c778836
Denis Defreyne <denis.defreyne@stoneship.org>
2014-08-30 19:41:47 +0200

    Add music

--- main.lua
+++ main.lua
@@ -6,6 +6,9 @@ function love.load()
   love.mouse.setGrabbed(true)
   love.graphics.setDefaultFilter('nearest')
 
+  music = love.audio.newSource('assets/wayfinder - 20140830 - minijam - blackbird.mp3')
+  music:play()
+
   -- ProFi:start()
   Gamestate.registerEvents()
   Gamestate.switch(TitleState.new())
