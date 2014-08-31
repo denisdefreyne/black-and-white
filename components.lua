5c12ab57e665cca0d7a0742b6a25115294fe99e0
Denis Defreyne <denis.defreyne@stoneship.org>
2014-08-31 18:43:42 +0200

    Remove unused format

--- components.lua
+++ components.lua
@@ -3,48 +3,39 @@ require('engine')
 local Components = {}
 
 Components.BlackPlayer = {
-  new    = function() return {} end,
-  format = function(self) return 'Yes' end,
+  new = function() return {} end,
 }
 
 Components.WhitePlayer = {
-  new    = function() return {} end,
-  format = function(self) return 'Yes' end,
+  new = function() return {} end,
 }
 
 Components.Gun = {
-  new    = function() return { maxCooldown = 0.4, curCooldown = 0.1 } end,
-  format = function(self) return 'Yes' end,
+  new = function() return { maxCooldown = 0.4, curCooldown = 0.1 } end,
 }
 
 Components.EnemySpawner = {
-  new    = function() return { maxCooldown = 3.0, curCooldown = 0, lifetime = 0 } end,
-  format = function(self) return 'Yes' end,
+  new = function() return { maxCooldown = 3.0, curCooldown = 0, lifetime = 0 } end,
 }
 
 Components.CollisionGroup = {
-  new    = function(name) return { name = name } end,
-  format = function(self) return '' .. name end,
+  new = function(name) return { name = name } end,
 }
 
 Components.OriginatingEntity = {
-  new    = function(entity) return { entity = entity } end,
-  format = function(self) return '' end,
+  new = function(entity) return { entity = entity } end,
 }
 
 Components.Health = {
-  new    = function(max) return { cur = max, max = max } end,
-  format = function(self) return '' end,
+  new = function(max) return { cur = max, max = max } end,
 }
 
 Components.HealthTracking = {
-  new    = function(entity) return { entity = entity } end,
-  format = function(self) return '' end,
+  new = function(entity) return { entity = entity } end,
 }
 
 Components.EnemyBehavior = {
-  new    = function() return { freq = (2 + math.random()) * 1.5, lifetime = math.random()*10 } end,
-  format = function(self) return '' end,
+  new = function() return { freq = (2 + math.random()) * 1.5, lifetime = math.random()*10 } end,
 }
 
 return Components
